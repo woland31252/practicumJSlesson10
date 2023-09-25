eb99f4656ecbcb0f7d107d278dda7761ffdf09a0
woland31252 <110925314+woland31252@users.noreply.github.com>
2023-09-25 18:10:12 +0000

    Deploying to gh-pages from @ woland31252/practicumJSlesson10@ce4d2cd31cc480842347dfe268dc9e10cbf5d4df 🚀

--- index.35bae1c0.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={headers:{"x-api-key":"live_eFQNzE6t9Yns8DpBqOnhUKvvvHPrVTd4nqMp4HJQUHmfMUGURXupaEMIop0eMOzH"}};var t;document.querySelector(".breed-select");(t="".concat("https://api.thecatapi.com/v1","/breeds"),fetch(t,e).then((function(e){if(!e.ok)throw new Error(e.status);return e.json()}))).then((function(e){return console.log(e)}))}();
-//# sourceMappingURL=index.35bae1c0.js.map
--- index.5e47c332.js
+++
@@ -1,2 +0,0 @@
-const e={headers:{"x-api-key":"live_eFQNzE6t9Yns8DpBqOnhUKvvvHPrVTd4nqMp4HJQUHmfMUGURXupaEMIop0eMOzH"}};document.querySelector(".breed-select");fetch("https://api.thecatapi.com/v1/breeds",e).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((e=>console.log(e)));
-//# sourceMappingURL=index.5e47c332.js.map

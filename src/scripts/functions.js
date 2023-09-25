ce4d2cd31cc480842347dfe268dc9e10cbf5d4df
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-09-25 21:09:42 +0300

    add_style.css, update_funstions.js

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -1,8 +1,29 @@
 import fetchBreeds from "./api.js";
 
 const select = document.querySelector(".breed-select");
-fetchBreeds().then(data => console.log(data));
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
-function createBreedList(data) {
-    data.reduce(marcup, elem )
+loader.classList.toggle("is-hidden");
+
+fetchBreeds()
+  .then(data => {
+    if (data.length === 0) throw new Error('No data');
+    return data.reduce((markup, elem) => createMarkup(elem) + markup, '');
+  })
+  .then(updateBreedList)
+  .catch(onError);
+
+
+function updateBreedList(markup) {
+    select.innerHTML = markup;
+    loader.classList.toggle('is-hidden');
+}
+
+function createMarkup({ id, name }) {
+    return `<option id="${id}">${name}</option>`;
+}
+
+function onError() {
+    error.classList.toggle('is-hidden');
 }

c6b42b82ba6e77f38cbd274bf9278e4b33f69761
woland31252 <110925314+woland31252@users.noreply.github.com>
2023-09-25 16:20:09 +0000

    Deploying to gh-pages from @ woland31252/practicumJSlesson10@b830a6470beac8892b9ddb59a219c83b0573528d 🚀

--- index.9a74d807.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds",{headers:{"x-api-key":"live_eFQNzE6t9Yns8DpBqOnhUKvvvHPrVTd4nqMp4HJQUHmfMUGURXupaEMIop0eMOzH"}}).then((e=>e.json())).then((e=>console.log(e)));
-//# sourceMappingURL=index.9a74d807.js.map

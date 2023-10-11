3ac83a985ac385d5d34a86f72cec8c345f944a80
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-10-11 23:17:39 +0300

    Update cat-api.js

--- src/scripts/cat-api.js
+++ src/scripts/cat-api.js
@@ -1,11 +1,9 @@
 // import axios from 'axios';
-// const instance = axios.create({
-//   baseURL: 'https://api.thecatapi.com/v1/breeds',
-// });
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_eFQNzE6t9Yns8DpBqOnhUKvvvHPrVTd4nqMp4HJQUHmfMUGURXupaEMIop0eMOzH';
 
 const url = `https://api.thecatapi.com/v1`;
-// axios.defaults.headers.common['x-api-key'];
-// ('live_eFQNzE6t9Yns8DpBqOnhUKvvvHPrVTd4nqMp4HJQUHmfMUGURXupaEMIop0eMOzH');
+
 const key = {
   headers: {
     'x-api-key':
@@ -17,6 +15,13 @@ const key = {
 
 function fetchBreeds() {
   const breeds = `${url}/breeds`
+//   return axios.get(breeds).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status)
+//     } return response.data;
+//   });
+// }
+
   return fetch(breeds, key)
     .then(response => {
       if (!response.ok) {
@@ -24,6 +29,16 @@ function fetchBreeds() {
       } return response.json()
     });
 }
+
+// function fetchCatByBreed(breedId) {
+// const breed = `${url}/images/search?breed_ids=${breedId}`;
+// return axios.get(breed).then(response => {
+//   if (!response.ok) {
+//     throw new Error(response.status);
+//   }
+//   return response.data;
+// });
+// }
 
 function fetchCatByBreed(breedId) {
 const breed = `${url}/images/search?breed_ids=${breedId}`;

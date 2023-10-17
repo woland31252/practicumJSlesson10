0378956f8d5347564fb17f1d9a5852d8ab5c926d
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-10-17 21:03:38 +0300

    not axios

--- src/scripts/cat-api.js
+++ src/scripts/cat-api.js
@@ -1,28 +1,21 @@
-import axios from 'axios';
-axios.defaults.headers.common['x-api-key'] =
-  'live_eFQNzE6t9Yns8DpBqOnhUKvvvHPrVTd4nqMp4HJQUHmfMUGURXupaEMIop0eMOzH';
+// import axios from 'axios';
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_eFQNzE6t9Yns8DpBqOnhUKvvvHPrVTd4nqMp4HJQUHmfMUGURXupaEMIop0eMOzH';
 
 const url = 'https://api.thecatapi.com/v1';
 
-// const key = {
-//   headers: {
-//     'x-api-key':
-//       'live_eFQNzE6t9Yns8DpBqOnhUKvvvHPrVTd4nqMp4HJQUHmfMUGURXupaEMIop0eMOzH',
-//   },
-// };
+const key = {
+  headers: {
+    'x-api-key':
+      'live_eFQNzE6t9Yns8DpBqOnhUKvvvHPrVTd4nqMp4HJQUHmfMUGURXupaEMIop0eMOzH',
+  },
+};
 
 
 
 function fetchBreeds() {
   const breeds = `${url}/breeds`
-//   return axios.get(breeds).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status)
-//     } return response.data;
-//   });
-// }
-
-  return fetch(breeds, axios)
+  return fetch(breeds, key)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status)
@@ -30,19 +23,9 @@ function fetchBreeds() {
     });
 }
 
-// function fetchCatByBreed(breedId) {
-// const breed = `${url}/images/search?breed_ids=${breedId}`;
-// return axios.get(breed).then(response => {
-//   if (!response.ok) {
-//     throw new Error(response.status);
-//   }
-//   return response.data;
-// });
-// }
-
 function fetchCatByBreed(breedId) {
 const breed = `${url}/images/search?breed_ids=${breedId}`;
-return fetch(breed, axios).then(response => {
+return fetch(breed, key).then(response => {
   if (!response.ok) {
     throw new Error(response.status);
   }
